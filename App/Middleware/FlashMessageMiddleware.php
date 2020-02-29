a23c65a33bbdacc74c8095a4bea7b918fc67644b
Yaschenko Sergey <yaschenkoserge1@gmail.com>
2020-02-29 15:41:37 +0600

    add FlashMessesageMiddleware

---
+++ App/Middleware/FlashMessageMiddleware.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Middleware;
+
+
+use App\Router\Route;
+
+class FlashMessageMiddleware implements IMiddleware
+{
+
+    public function run(Route $route)
+    {
+        // TODO: Implement run() method.
+    }
+}
