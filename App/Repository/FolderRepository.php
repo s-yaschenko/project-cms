6d1ac9463d2e5ffb0d48fbe41e3bd9f8b0170134
Yaschenko Sergey <yaschenkoserge1@gmail.com>
2020-03-03 12:33:55 +0600

    add repository FolderRepository

---
+++ App/Repository/FolderRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Repository;
+
+
+use App\Model\Folder;
+
+class FolderRepository extends AbstractRepository
+{
+    /**
+     * @var string
+     */
+    protected $model = Folder::class;
+}
